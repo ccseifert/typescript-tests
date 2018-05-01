5eb696a377077a50e358ed24b49042fc874d2b2b
Unknown <ccseifert@gmail.com>
2018-04-30 20:35:36 -0400

    Interface tests

---
+++ interface-sample/interface.ts
@@ -0,0 +1,31 @@
+// basic interface
+interface LabelledValue {
+    label: string;
+}
+
+function printLabel(labelledObj: LabelledValue) {
+    console.log(labelledObj.label);
+}
+
+let myObj = {size: 10, label: "Size 10 Object"};
+printLabel(myObj);
+
+// optional properties
+interface SquareConfig {
+    color?: string;
+    width?: number;
+}
+
+function createSquare(config: SquareConfig): {color: string; area: number} {
+    let newSquare = {color: "white", area: 100};
+    if (config.color) {
+        newSquare.color = config.color;
+    }
+    if (config.width) {
+        newSquare.area = config.width * config.width;
+    }
+    return newSquare;
+}
+
+let mySquare = createSquare({color: "black"});
+console.log(mySquare);
